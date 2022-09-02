2072821d5c2433c2317352c65aae1c28cfc5d5df
Akana <Akana92@gmail.com>
2022-09-02 11:35:15 +0600

    Add project files.

---
+++ guessNumber/Form2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace guessNumber
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+            this.Text = "Введите число";
+        }
+
+        public int userAnswer
+        {
+            get { return int.Parse(tboxAnswer.Text); }
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tboxAnswer_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
